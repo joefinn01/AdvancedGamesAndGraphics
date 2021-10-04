37313a7f00904658fa60a9dc32abbd91383a1958
Joe Finn <joefinn01@hotmail.com>
2021-10-05 00:08:47 +0100

    Adding window manager

---
+++ AdvancedGamesAndGraphics/Engine/Managers/WindowManager.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Engine/Structure/Singleton.h"
+
+#include <dxgitype.h>
+#include <Windows.h>
+
+typedef DXGI_MODE_DESC WindowSettings;
+
+class WindowManager : public Singleton<WindowManager>
+{
+public:
+
+	//Getters and setters
+	void SetHWND(HWND hwnd);
+	HWND GetHWND() const;
+
+	void SetRefreshRate(UINT uiNumerator, UINT uiDenominator);
+	void GetRefreshRate(UINT& uiNumerator, UINT& uiDenominator) const;
+
+	void SetWindowDimensions(UINT uiWidth, UINT uiHeight);
+
+	void SetWindowWidth(UINT uiWidth);
+	UINT GetWindowWidth() const;
+
+	void SetWindowHeight(UINT uiHeight);
+	UINT GetWindowHeight() const;
+
+	void SetWindowSettings(WindowSettings settings);
+
+	float GetAspectRatio() const;
+
+protected:
+
+private:
+	HWND m_HWND = nullptr;
+
+	WindowSettings m_WindowSettings = WindowSettings();
+};
