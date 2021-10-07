254a5bd266fa27fb9fefbe6210c8ed0469db3377
Joe Finn <joefinn01@hotmail.com>
2021-10-07 15:49:06 +0100

    Moving helper function to more suitable location (Shader manager)

--- AdvancedGamesAndGraphics/Engine/Helpers/DirectXHelper.h
+++ AdvancedGamesAndGraphics/Engine/Helpers/DirectXHelper.h
@@ -8,7 +8,7 @@ class DirectXHelper
 {
 public:
 	static Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(ID3D12Device4* pDevice, ID3D12GraphicsCommandList* pGraphicsCommandList, const void* pData, UINT64 uiByteSize, Microsoft::WRL::ComPtr<ID3D12Resource>& pUploadBuffer);
-	static Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(const std::wstring& kwsFilename, const std::string& ksName, const D3D_SHADER_MACRO* pDefines, const std::string& ksEntrypoint, const std::string& ksTarget);
+
 protected:
 
 private:
