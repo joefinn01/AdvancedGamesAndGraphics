4031786a4544b590bf86c0401afb8efe8f6fff94
Joe Finn <joefinn01@hotmail.com>
2021-10-15 11:05:40 +0100

    Updating material variables

--- AdvancedGamesAndGraphics/Engine/DirectX/Material.h
+++ AdvancedGamesAndGraphics/Engine/DirectX/Material.h
@@ -8,7 +8,8 @@ struct Material
 {
 	std::string name;
 	UINT CBIndex;
-	DirectX::XMFLOAT4 diffuse;
-	DirectX::XMFLOAT3 fresnel;
-	float roughness;
+
+	DirectX::XMFLOAT4 Ambient;
+	DirectX::XMFLOAT4 Diffuse;	//4th float is the alpha
+	DirectX::XMFLOAT4 Specular;	//4th float is the specular power
 };
