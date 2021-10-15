5f515a6d63b658ab95d26e85af73d1fb595b2369
Joe Finn <joefinn01@hotmail.com>
2021-10-15 16:05:51 +0100

    Updating vertices to take texcoords

--- AdvancedGamesAndGraphics/Engine/DirectX/Vertices.h
+++ AdvancedGamesAndGraphics/Engine/DirectX/Vertices.h
@@ -6,22 +6,33 @@ struct Vertex
 {
 	DirectX::XMFLOAT3 position;
 	DirectX::XMFLOAT3 normal;
+	DirectX::XMFLOAT2 TexCoords;
+
+	Vertex(DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 norm, DirectX::XMFLOAT2 texCoords)
+	{
+		position = pos;
+		normal = norm;
+		TexCoords = texCoords;
+	}
 
 	Vertex(DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 norm)
 	{
 		position = pos;
 		normal = norm;
+		TexCoords = DirectX::XMFLOAT2(0, 0);
 	}
 
 	Vertex(DirectX::XMFLOAT3 pos)
 	{
 		position = pos;
 		normal = DirectX::XMFLOAT3(0, 0, 0);
+		TexCoords = DirectX::XMFLOAT2(0, 0);
 	}
 
 	Vertex()
 	{
 		position = DirectX::XMFLOAT3(0, 0, 0);
 		normal = DirectX::XMFLOAT3(0, 0, 0);
+		TexCoords = DirectX::XMFLOAT2(0, 0);
 	}
 };
